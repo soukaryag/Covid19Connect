d3e0f1d84de11362c53e864044135a77d716f738
soukaryag <sg4fz@virginia.edu>
2020-09-22 22:43:01 -0400

    remove secret key from repo

--- covidtweets/settings.py
+++ covidtweets/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o@gk1-%u_ohnuso6on7+ydyji8c9)%+1j)j@oi$)+&0yq960y&'
+SECRET_KEY = os.getenv('COVIDTWEETS_SECRET_KEY', 'null').replace(']', ')').replace('~', '&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
